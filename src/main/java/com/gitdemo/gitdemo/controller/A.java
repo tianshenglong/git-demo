2070c2c27ed57a132878720137862bf84b55bb13
tianshenglong <tianshenglongchang@126.com>
2018-06-02 17:27:52 +0800

    测试

--- src/main/java/com/gitdemo/gitdemo/controller/A.java
+++ src/main/java/com/gitdemo/gitdemo/controller/A.java
@@ -1,4 +1,16 @@
 package com.gitdemo.gitdemo.controller;
 
 public class A {
+
+    public void add(){
+        System.out.println("tianjia");
+    }
+
+
+
+//    sdfsdfs
+
+    public void adda(){
+        System.out.println("tianjia");
+    }
 }
