94bfc8254ac80d312583b779f7a324760e486528
tianshenglong <tianshenglongchang@126.com>
2018-06-02 20:01:28 +0800

    Update B.java

--- src/main/java/com/gitdemo/controller1/B.java
+++ src/main/java/com/gitdemo/controller1/B.java
@@ -1,7 +1,14 @@
 package com.gitdemo.controller1;
 
 public class B {
-    public void a(){
+    public void a1(){
+        
         System.out.println("aa");
+        System.out.println("ab");
+    }
+    public void b(){
+        
+        System.out.println("aa");
+        System.out.println("ab");
     }
 }
