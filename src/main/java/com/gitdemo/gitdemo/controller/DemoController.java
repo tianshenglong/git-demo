e4c01ed55d8f026b83dca93cab447b2cb8e07f1a
tianshenglong <tianshenglongchang@126.com>
2018-07-24 19:12:55 +0800

    init commit

---
+++ src/main/java/com/gitdemo/gitdemo/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.gitdemo.gitdemo.controller;
+
+/**
+ * @Author: tianshenglong
+ * @Date: 2018/7/24 19:10
+ */
+public class DemoController {
+
+    public static void main(String[] args) {
+
+    }
+}
